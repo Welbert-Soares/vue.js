5b29e0b50fb81068555ff72c315ff81e37335031
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-03-26 19:40:06 -0300

    desafio concluido

--- dom/desafio-02/app.js
+++ dom/desafio-02/app.js
@@ -2,5 +2,13 @@ new Vue({
     el: '#desafio',
     data: {
         valor: ''
+    },
+    methods: {
+        showAlert() {
+            alert('Você clicou!')
+        },
+        storageKeydown(event) {
+           this.valor = event.target.value
+        }
     }
 })
