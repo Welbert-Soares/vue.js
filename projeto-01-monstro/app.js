028e6bd65dd9701ce21ed91ab934ccc3293e01aa
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-08 12:08:22 -0300

    adicionando registro de log

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -4,6 +4,7 @@ new Vue({
     running: false,
     playerLife: 100,
     monsterLife: 100,
+    logs: [],
   },
   computed: {
     hasResult() {
@@ -18,13 +19,16 @@ new Vue({
     },
 
     attack(especial) {
-      this.hurt("playerLife", 7, 12, false);
-      this.hurt("monsterLife", 5, 10, especial);
+      this.hurt("monsterLife", 5, 10, especial, "Jogador", "Monstro", "player");
+      if (this.monsterLife > 0) {
+        this.hurt("playerLife", 7, 12, false, "Monstro", "Jogador", "monster");
+      }
     },
-    hurt(atributes, min, max, especial) {
+    hurt(atributes, min, max, especial, source, target, clas) {
       const plus = especial ? 5 : 0;
       const hurt = this.getRandom(min + plus, max + plus);
       this[atributes] = Math.max(this[atributes] - hurt, 0);
+      this.registerLog(`${source} atingiu ${target} com ${hurt} de dano.`, clas);
     },
     healAndHurt() {
       this.heal(10, 15);
@@ -37,6 +41,9 @@ new Vue({
     getRandom(min, max) {
       const value = Math.random() * (max - min) + min;
       return Math.round(value);
+    },
+    registerLog(text, clas) {
+      this.logs.unshift(text, clas);
     },
   },
   watch: {
