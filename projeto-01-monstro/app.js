f8b82a53bb97c48b913636460d67a1aaaacff785
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-08 11:55:40 -0300

    implementando cura

--- projeto-01-monstro/app.js
+++ projeto-01-monstro/app.js
@@ -1,39 +1,47 @@
 new Vue({
-    el: '#app',
-    data: {
-        running: false,
-        playerLife: 100,
-        monsterLife: 100,
+  el: "#app",
+  data: {
+    running: false,
+    playerLife: 100,
+    monsterLife: 100,
+  },
+  computed: {
+    hasResult() {
+      return this.playerLife == 0 || this.monsterLife == 0;
     },
-    computed: {
-        hasResult() {
-            return this.playerLife == 0 || this.monsterLife == 0 
-        }
+  },
+  methods: {
+    startGame() {
+      this.running = true;
+      this.playerLife = 100;
+      this.monsterLife = 100;
     },
-    methods: {
-        startGame() {
-            this.running = true
-            this.playerLife = 100
-            this.monsterLife = 100
-        },
-        
-        attack(especial) {
-            this.hurt('playerLife', 7, 12, false)
-            this.hurt('monsterLife',5, 10, especial)
-        },
-        hurt(atributes, min, max, especial) {
-            const plus = especial ? 5 : 0
-            const hurt = this.getRandom(min + plus, max + plus)
-            this[atributes] = Math.max(this[atributes] - hurt, 0)
-        },
-        getRandom(min, max) {
-            const value = Math.random() * (max - min) + min
-            return Math.round(value)
-        }
+
+    attack(especial) {
+      this.hurt("playerLife", 7, 12, false);
+      this.hurt("monsterLife", 5, 10, especial);
     },
-    watch: {
-        hasResult(value) {
-            if (value) this.running = false
-        }
-    }
-})
+    hurt(atributes, min, max, especial) {
+      const plus = especial ? 5 : 0;
+      const hurt = this.getRandom(min + plus, max + plus);
+      this[atributes] = Math.max(this[atributes] - hurt, 0);
+    },
+    healAndHurt() {
+      this.heal(10, 15);
+      this.hurt("playerLife", 7, 12, false);
+    },
+    heal(min, max) {
+      const heal = this.getRandom(min, max);
+      this.playerLife = Math.min(this.playerLife + heal, 100);
+    },
+    getRandom(min, max) {
+      const value = Math.random() * (max - min) + min;
+      return Math.round(value);
+    },
+  },
+  watch: {
+    hasResult(value) {
+      if (value) this.running = false;
+    },
+  },
+});
