d23db4623333eaa0d37c21d24d301f466402c67c
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-09 11:42:05 -0300

    criando um componente

--- componentes/componente-intro/src/main.js
+++ componentes/componente-intro/src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
+import ContadorComponent from './ContadorComponent.vue'
 
 Vue.config.productionTip = false
+Vue.component('app-contador', ContadorComponent)
 
 new Vue({
   render: h => h(App),
