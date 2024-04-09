3e37c38fb072aab4bea5f2684294ca952a2077b4
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-09 11:02:09 -0300

    criando projeto com vue cli

---
+++ componentes/componente-intro/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <h1>Contadores {{ contador }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+  data() {
+    return {
+      contador: 0
+    }
+  }
+}
+</script>
+
+<style></style>
