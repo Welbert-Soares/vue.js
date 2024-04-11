10e9bde508e16fe76e9b7d7fc73940c39c6f5926
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-11 12:02:46 -0300

    compunente newtask

--- projeto-02-todo/tarefas-todo/src/components/NewTask.vue
+++ projeto-02-todo/tarefas-todo/src/components/NewTask.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="new-task">
-    <input v-model="name" @keydown.enter="addTask" type="text">
-    <button @click="addTask">+</button>
+    <input v-model="name" @keydown.enter="addTask" type="text"
+        class="form-element" placeholder="Nova tarefa?">
+    <button class="form-element"  @click="addTask">+</button>
   </div>
 </template>
 
@@ -24,5 +25,36 @@ export default {
 </script>
 
 <style>
+    .new-task {
+        margin: 35px;
 
+    }
+
+    .form-element {
+        outline: none;
+        font-size: 2rem;
+        border: 1px solid #fff;
+        padding: 5px 10px 8px;
+        color: #fff;
+
+    }
+
+    input.form-element {
+        width: 500px;
+        background-color: #fff2;
+        border-top-left-radius: 8px;
+        border-bottom-left-radius: 8px;
+    }
+
+    button.form-element {
+        border-left: none;
+        background-color: #2196f3;
+        border-top-right-radius: 8px;
+        border-bottom-right-radius: 8px;
+        cursor: pointer;
+    }
+
+    button.form-element:hover {
+        background-color: #178fff;
+    }
 </style>
