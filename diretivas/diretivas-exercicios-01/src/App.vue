7ab619cd83c74660292ba5c80969162bffd0a004
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-19 08:58:33 -0300

    registrando diretivas localmente

--- diretivas/diretivas-exercicios-01/src/App.vue
+++ diretivas/diretivas-exercicios-01/src/App.vue
@@ -1,18 +1,40 @@
 <template>
 	<div id="app">
 		<h1>Diretivas</h1>
-    <hr>
-    <p v-text="'Usando diretiva v-text'"></p>
-    <p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
-	<hr>
-	<!-- <p v-nome:argumento.mod1.mod2.mod3 ="..."></p> -->
-	<p v-destaque:fundo.atrasar="'red'">Usando diretiva personalizada</p>
-	<p v-destaque="cor">Usando diretiva personalizada</p>
+		<hr>
+		<p v-text="'Usando diretiva v-text'"></p>
+		<p v-html="'Usando diretiva <strong>v-html</strong>'"></p>
+		<hr>
+		<!-- <p v-nome:argumento.mod1.mod2.mod3 ="..."></p> -->
+		<p v-destaque:fundo.atrasar="'red'">Usando diretiva personalizada</p>
+		<p v-destaque="cor">Usando diretiva personalizada</p>
+		<p v-destaque-local:fundo.atrasar="'red'">Usando diretiva personalizada</p>
+		<p v-destaque-local="cor">Usando diretiva personalizada</p>
 	</div>
 </template>
 
 <script>
+
 export default {
+	component: {},
+	directives: {
+		'destaque-local': {
+			bind(el, binding) {
+				// el.style.backgroundColor = 'lightgreen'
+
+				let atraso = 0;
+				if (binding.modifiers["atrasar"]) atraso = 3000;
+
+				setTimeout(() => {
+					if (binding.arg === "fundo") {
+						el.style.backgroundColor = binding.value;
+					} else {
+						el.style.color = binding.value;
+					}
+				}, atraso);
+			},
+		}
+	},
 	data() {
 		return {
 			cor: 'blue'
