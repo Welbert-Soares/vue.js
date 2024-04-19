6cf64e039c68fb6f425ab959b5429d32888e9c67
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-19 14:17:29 -0300

    fade css

--- animacoes/animacoes-exercicios/src/App.vue
+++ animacoes/animacoes-exercicios/src/App.vue
@@ -1,13 +1,25 @@
 <template>
 	<div id="app" class="container-fluid">
 		<h1>Animações</h1>
+    <hr>
+    <button class="btn"
+      @click="exibir = !exibir">Mostrar Mensagem</button>
+
+    <transition name="fade">
+      <div class="alert" v-if="exibir">{{ msg }}</div>
+    </transition>
 	</div>
 </template>
 
 <script>
 
 export default {
-
+  data() {
+    return {
+      msg: 'Uma mensagem de informação para o usuário!',
+      exibir: false,
+    }
+  }
 }
 </script>
 
@@ -21,4 +33,36 @@ export default {
 	margin-top: 60px;
 	font-size: 1.5rem;
 }
+
+.btn {
+  width: 200px;
+  height: 50px;
+  padding: 10px 20px;
+  background-color: #187db8;
+  color: white;
+  border: 1px solid #0b537c;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background-color: #0b537c;
+}
+
+.alert {
+  padding: 20px;
+  margin-top: 20px;
+  background-color: #1b8f8f;
+  color: white;
+  border-radius: 5px;
+}
+
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 1.5s;
+}
+
 </style>
