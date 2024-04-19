2fff2f107c3f672bcc3bf9a569320e926b9857a0
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-19 13:49:17 -0300

    refatoramento legado animacoes

--- animacoes/animacoes-exercicios/src/main.js
+++ animacoes/animacoes-exercicios/src/main.js
@@ -1,10 +1,8 @@
-import '@babel/polyfill'
 import Vue from 'vue'
-import './plugins/bootstrap-vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-	render: h => h(App),
+  render: h => h(App),
 }).$mount('#app')
