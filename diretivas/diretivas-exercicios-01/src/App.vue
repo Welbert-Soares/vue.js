8e17bed851f808dbf5dd429f951124952190004f
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-19 09:25:24 -0300

    multiplos modificadores

--- diretivas/diretivas-exercicios-01/src/App.vue
+++ diretivas/diretivas-exercicios-01/src/App.vue
@@ -8,8 +8,8 @@
 		<!-- <p v-nome:argumento.mod1.mod2.mod3 ="..."></p> -->
 		<p v-destaque:fundo.atrasar="'red'">Usando diretiva personalizada</p>
 		<p v-destaque="cor">Usando diretiva personalizada</p>
-		<p v-destaque-local:fundo.atrasar="'red'">Usando diretiva personalizada</p>
-		<p v-destaque-local="cor">Usando diretiva personalizada</p>
+		<p v-destaque-local:fundo.atrasar.alternar="'red'">Usando diretiva personalizada</p>
+		<p v-destaque-local.atrasar="cor">Usando diretiva personalizada</p>
 	</div>
 </template>
 
@@ -20,16 +20,29 @@ export default {
 	directives: {
 		'destaque-local': {
 			bind(el, binding) {
-				// el.style.backgroundColor = 'lightgreen'
+				const aplicarCor = cor => {
+					if (binding.arg === "fundo") {
+						el.style.backgroundColor = cor;
+					} else {
+						el.style.color = cor;
+					}
+				};
 
 				let atraso = 0;
 				if (binding.modifiers["atrasar"]) atraso = 3000;
 
+				const cor1 = binding.value;
+				const cor2 = "yellow";
+				let corAtual = cor1;
+
 				setTimeout(() => {
-					if (binding.arg === "fundo") {
-						el.style.backgroundColor = binding.value;
+					if (binding.modifiers["alternar"]) {
+						setInterval(() => {
+							corAtual = corAtual === cor1 ? cor2 : cor1;
+							aplicarCor(corAtual);
+						}, 1000);
 					} else {
-						el.style.color = binding.value;
+						aplicarCor(binding.value)
 					}
 				}, atraso);
 			},
