6090bff645fdc9d84497e4d035f8c5b91681a9bd
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-04-22 10:09:53 -0300

    enviando post

--- http/http-exercicios/src/App.vue
+++ http/http-exercicios/src/App.vue
@@ -1,12 +1,49 @@
 <template>
 	<div id="app" class="container">
 		<h1>HTTP com Axios</h1>
+		<div class="mb-3">
+			<label for="nomeInput" class="form-label ">Nome:</label>
+			<input type="text" class="form-control fs-5" id="nomeInput" placeholder="Informe seu Nome"
+				v-model="usuario.nome">
+		</div>
+		<div class="mb-3">
+			<label for="emailInput" class="form-label fs-3">E-mail:</label>
+			<input type="email" class="form-control fs-5" id="emailInput" placeholder="Informe seu E-mal"
+				v-model="usuario.email">
+		</div>
+		<button @click.prevent="salvar" class="btn btn-primary ">Enviar</button>
 	</div>
 </template>
 
 <script>
-export default {
 
+export default {
+	data() {
+		return {
+			usuario: {
+				nome: '',
+				email: ''
+			}
+		}
+	},
+	methods: {
+		salvar() {
+			this.$http.post('usuarios.json', this.usuario)
+				.then(resp => {
+					console.log(resp)
+					this.usuario = {
+						nome: '',
+						email: ''
+					}
+				})
+		}
+	}
+	// created() {
+	// 	this.$http.post('usuarios.json', {
+	// 		nome: 'Welbert',
+	// 		email: 'welbertsoares@test.com'
+	// 	}).then(res => console.log(res))
+	// }
 }
 </script>
 
--- http/http-exercicios/src/main.js
+++ http/http-exercicios/src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+
+axios.defaults.baseURL = 'https://curso-vue-67e5a-default-rtdb.firebaseio.com/'
+
+app.config.globalProperties.$http = axios
+
+app.mount('#app')
